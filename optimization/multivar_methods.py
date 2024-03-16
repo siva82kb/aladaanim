a549d810b867777a8435f9a5fbc076b55e79547a
Sivakumar Balasubramanian <siva82kb@users.noreply.github.com>
2024-03-16 10:57:00 +0530

    Revert "Testing pull request"

--- optimization/multivar_methods.py
+++ optimization/multivar_methods.py
@@ -11,6 +11,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
 import aladaopt
 
 import platform
